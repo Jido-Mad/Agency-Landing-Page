ae0797e091d26585e507117f6cc5b51d17601e34
Madd Jido <smileboy1422@gmail.com>
2025-06-11 18:06:58 +0100

    fix

---
+++ src/Components/Attribution.jsx
@@ -0,0 +1,14 @@
+function Attribution() {
+  return (
+    <>
+      <div>
+        Challenge by
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Mounir</a>.
+      </div>
+    </>
+  );
+}
+export default Attribution;
