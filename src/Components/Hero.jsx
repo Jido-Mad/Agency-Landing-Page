0fec38851671c2555ac682eae2a0126b857cc76b
n1ro23 <angmunir@gmail.com>
2025-06-12 16:55:13 +0100

    hero done

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,7 +1,7 @@
 function Hero() {
   return (
-    <div className="bg-[url('./src/assets/images/mobile/image-header.jpg')] bg-cover bg-center h-screen w-full">
-      <header className="flex justify-between items-center p-4">
+    <div className="bg-[url('./src/assets/images/mobile/image-header.jpg')] bg-cover bg-center h-screen w-full md:bg-[url('./src/assets/images/desktop/image-header.jpg')]">
+      <header className="flex justify-between items-center p-6">
         <div>
           <img src="./logo.svg" alt="logo" className="w-32" />
         </div>
@@ -9,38 +9,56 @@ function Hero() {
           <img
             src="./icon-hamburger.svg"
             alt="hamburger-icon"
-            className="w-5 h-5"
+            className="w-6 h-5 block md:hidden"
           />
         </div>
-        {/*<nav>
-          <ul>
+        <nav className="hidden md:block pt-5">
+          <ul className="flex items-center gap-6 text-white font-barlow font-semibold uppercase text-sm tracking-widest">
             <li>
-              <a href="#about">About</a>
+              <a
+                href="#about"
+                className="hover:bg-white hover:text-black rounded-full transition-all duration-300 px-4 py-3"
+              >
+                About
+              </a>
             </li>
             <li>
-              <a href="#services">Services</a>
+              <a
+                href="#services"
+                className="hover:bg-white hover:text-black rounded-full transition-all duration-300 px-4 py-3"
+              >
+                Services
+              </a>
             </li>
             <li>
-              <a href="#projects">Projects</a>
+              <a
+                href="#projects"
+                className="hover:bg-white hover:text-black rounded-full transition-all duration-300 px-4 py-3"
+              >
+                Projects
+              </a>
             </li>
             <li>
-              <a href="#contact">Contact</a>
+              <a
+                href="#contact"
+                className="hover:bg-white hover:text-black rounded-full transition-all duration-300 px-4 py-3"
+              >
+                Contact
+              </a>
             </li>
           </ul>
-        </nav>*/}
+        </nav>
       </header>
 
       <div className="flex-1 flex flex-col items-center justify-center text-white text-center">
-        <h1
-          className="uppercase text-3xl mt-40 mb-6 fa"
-          style={{ fontFamily: "Fraunces, serif" }}
-        >
-          We are creatives
+        <h1 className="text-5xl mt-32 md:text-6xl font-black uppercase text-center tracking-[0.2em] leading-tight fraunces">
+          <span className="block md:inline">We are</span>{" "}
+          <span className="block md:inline">creatives</span>
         </h1>
         <img
           src="./icon-arrow-down.svg"
           alt="arrow-down"
-          className="w-8 h-30 mt-20"
+          className="w-8 h-36 mt-20 md:mt-12 md:w-8 md:h-28"
         />
       </div>
     </div>
