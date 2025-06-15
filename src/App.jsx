dc395cdb544b2b11b2df01586df855b6572675f6
Madd Jido <smileboy1422@gmail.com>
2025-06-15 14:49:39 +0100

    Added Testimonials Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Attribution from "./Components/Attribution.jsx";
 import Hero from "./Components/Hero.jsx";
 import Cards from "./Components/Cards.jsx";
+import Testimonals from "./Components/Testimonials.jsx";
 function App() {
   return (
     <>
       <Hero />
       <Cards />
+      <Testimonals />
       <Attribution />
     </>
   );
