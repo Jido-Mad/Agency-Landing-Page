bb9a7cc5806333b96b031c8ac4bfe0663f188b40
n1ro23 <angmunir@gmail.com>
2025-06-15 17:19:18 +0100

    miw

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Attribution from "./Components/Attribution.jsx";
 import Hero from "./Components/Hero.jsx";
 import Cards from "./Components/Cards.jsx";
+import Minicards from "./Components/Minicards.jsx";
+import Footer from "./Components/Footer.jsx";
 function App() {
   return (
     <>
       <Hero />
       <Cards />
+      <Minicards />
+      <Footer />
       <Attribution />
     </>
   );
--- src/Components/Attribution.jsx
+++ src/Components/Attribution.jsx
@@ -1,12 +1,13 @@
 function Attribution() {
   return (
     <>
-      <div>
+      <div className="text-center">
         Challenge by
         <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
           Frontend Mentor
         </a>
-        . Coded by <a href="#">Mounir</a>.
+        . Coded by{" "}
+        <a href="#">Mounir & wahd lhmar mn9oulouch asmo bsh myz3fch madjid</a>.
       </div>
     </>
   );
