f941a7257974d8ede555a1608f4c10d6b60f3db6
Madd Jido <smileboy1422@gmail.com>
2025-06-15 18:09:55 +0100

    Fixing deploy

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/agency-landing-page/",
+  base: "/Agency-Landing-Page",
 });
